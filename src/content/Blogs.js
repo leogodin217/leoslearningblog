261feb440a14cb3c9c1500c1245281e12ea0c35f
Leo Godin <leogodin217@gmail.com>
2016-10-24 07:50:59 -0700

    Added a new post

--- src/content/Blogs.js
+++ src/content/Blogs.js
@@ -15,6 +15,11 @@ const Blogs = () => {
 			id: 3,
 			title: "Starting with React",
 			content: "I'm not a fan of video tutorials, but React for Beginners comes highly recommended. So I bought the basic package from https://reactforbeginners.com/. After a couple hours, I'm starting to get React a bit. I have already covered the main features of ES2015, so I can at least read the code. JSX is weird, but useful as long as you have the right plugins for your text editor.</p> With React, you have to get rid of your concept of MVC, and realize that you are simply makeing components for your app, then rendering them.</p> I also learned that figuring out the JavaScript build tools on your own is a lot of work. Fortunately, we have a Heroku build pack that will easily laaunch your React app on Heroku with zero configuration. You can read about it here: https://blog.heroku.com/deploying-react-with-zero-configuration.</p>Now for more learning, before updating this blog."
+		},
+		{
+			id: 4,
+			title: "A Bit of ES2015 and React State Management",
+			content: "One of the problems I face in larning a framework in react is my deficient knowledge of JavaScript. I have used JavaScript in the past, but usually just enough JQuery to get by. Now, React introduces me to ES2015, before I truly understand JavaScript. Through the React For Beginners class, I am gaining a better understanding of context binding, and learning a few tricks along the way. Reading about JavaScript binding really made the concept of late binding hit home. I never truly understood the concept until now."
 		}
 
 	]
